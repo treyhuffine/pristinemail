26f7576eb66ab1f557d50a0786e2f12ebd176470
Trey Huffine <trey@postmates.com>
2017-01-05 15:41:13 -0800

    Bump version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { validateFile, validateUrl } from './validate-email';
 import { markdownToHtml, baseHtml } from './markdown-html';
+import { parseHtml, validateNodeStyle } from './html-parser';
 
 const pristinemail = {
   baseHtml,
   markdownToHtml,
+  parseHtml,
   validateFile,
+  validateNodeStyle,
   validateUrl
 };
 
